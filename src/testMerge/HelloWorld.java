7acb47ed6a8d382c8d60579156f9b88f46bdb208
fuqilin <xiao.weii@hotmail.com>
2015-10-27 00:44:45 +0800

    add something on mac

--- src/testMerge/HelloWorld.java
+++ src/testMerge/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("Hello World!");
 		System.out.println("Second push: master merge mac");
+		System.out.println("Add something on mac");
 	}
 }
