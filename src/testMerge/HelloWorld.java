21cf8e1c49b7bad07903399ec3b5a1b0351c35d0
fuqilin <xiao.weii@hotmail.com>
2015-10-26 22:47:39 +0800

    second push: push to master

--- src/testMerge/HelloWorld.java
+++ src/testMerge/HelloWorld.java
@@ -1,6 +1,8 @@
 package testMerge;
 
 public class HelloWorld {
-
-	
+	public static void main(String args[]){
+		System.out.println("Hello World!");
+		System.out.println("Second push: master merge mac");
+	}
 }
