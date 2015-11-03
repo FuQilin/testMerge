dbbbabef1ee9381c1da688d4b595df520955bbdb
fuqilin <xiao.weii@hotmail.com>
2015-11-04 02:32:43 +0800

    add macBranch in the main

--- src/testMerge/HelloWorld.java
+++ src/testMerge/HelloWorld.java
@@ -6,9 +6,10 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Second test: master merge mac");
 		System.out.println("Add something on mac");
+		macBranch();
 	}
 	
-	public void macBranch(){
+	public static void macBranch(){
 		System.out.println("This is mac branch.");
 	}
 }
